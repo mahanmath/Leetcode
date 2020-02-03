fc9f0b08702a254f0b24d0c459abbc436044204d
mahanmath <38890073+mahanmath@users.noreply.github.com>
2020-02-03 16:35:22 -0500

    Create 384. Shuffle an Array.py

---
+++ Algorithms/384. Shuffle an Array.py
@@ -0,0 +1,25 @@
+class Solution:
+    
+    def __init__(self, nums: List[int]):
+        self.original = nums
+    def reset(self) -> List[int]:
+        """
+        Resets the array to its original configuration and return it.
+        """
+        return(self.original)
+
+    def shuffle(self) -> List[int]:
+        """
+        Returns a random shuffling of the array.
+        """
+        import numpy as np
+        
+        return(np.random.permutation(self.original))
+        
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
